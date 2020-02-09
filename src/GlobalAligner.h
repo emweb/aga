5a928b66a9ab5e815c65f6c701c8addc54484d1d
Koen Deforche <koen.deforche@blooloc.com>
2020-02-09 16:10:18 +0100

    Fix scoring of ref gaps at start of sequence (fixes wrong alignments circular genomes)

--- src/GlobalAligner.h
+++ src/GlobalAligner.h
@@ -47,6 +47,7 @@ GlobalAligner<Scorer, Reference, Query, SideN>::align(const Reference& ref, cons
     unsigned hj = j + 1;
     result[hj].cigar = result[hj - 1].cigar;
     result[hj].cigar.addRefGap();
+    result[hj].score = result[hj - 1].score;
     if (j == 0)
       result[hj].score += scorer_.scoreOpenRefGap(ref, query, -1, 0);
     else
